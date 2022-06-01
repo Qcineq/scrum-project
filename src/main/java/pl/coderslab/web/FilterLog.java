a5614a95df55de6401c1e1a86549506c8ada07d9
Kamil Kopczyński <kopczynskikam@gmail.com>
2022-06-01 18:44:12 +0200

    Recipe & RecipeDao - CRUD

---
+++ src/main/java/pl/coderslab/web/FilterLog.java
@@ -0,0 +1,28 @@
+package pl.coderslab.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/recipes.html")
+public class FilterLog implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest req= (HttpServletRequest) request;
+
+        if (req.getSession().getAttribute("email") == null || req.getSession().getAttribute("password")==null) {
+            HttpServletResponse res = (HttpServletResponse) response;
+            res.sendRedirect("/login");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
