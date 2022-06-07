8207e9db145d88e987864fed12c50a93d12fa077
Kuba <jakub.kucinski@icloud.com>
2022-06-07 19:18:50 +0200

    Register

--- src/main/java/pl/coderslab/web/Register.java
+++ src/main/java/pl/coderslab/web/Register.java
@@ -1,5 +1,8 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -14,10 +17,25 @@ public class Register extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         String name = request.getParameter("name");
         String surname = request.getParameter("surname");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String repassword = request.getParameter("repassword");
+        HttpSession session = request.getSession();
+
+
+        if (name != "" && surname != "" && email != "" && password != "") {
+
+            AdminDao adminDao = new AdminDao();
+            response.sendRedirect("http://localhost:8080/login");
+
+        } else {
+            String wrongPassOrEmail = "Podano nieprawidłowe dane. Spróbuj ponownie";
+            request.setAttribute("wrongpass", wrongPassOrEmail);
+            request.getServletContext().getRequestDispatcher("/register.jsp").forward(request, response);
+        }
+
     }
 }
