4d9b24b79d8a6a7ef5ba004aa29235258d04c5db
mszyb <marcinszyb@gmail.com>
2022-06-09 23:04:04 +0200

    dodawanie przepisu do planu

--- src/main/java/pl/coderslab/web/AddRecipeToPlan.java
+++ src/main/java/pl/coderslab/web/AddRecipeToPlan.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.util.List;
 
 @WebServlet(name = "AddPlanToSchedule", value = "/app/recipe/plan/add")
-public class AddPlanToSchedule extends HttpServlet {
+public class AddRecipeToPlan extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf8");
